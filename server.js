a9bf3e13c21cd8137ab296ee5e341c3b8007838f
mustafa <mokrani.mustafa@gmail.com>
2018-05-29 23:15:53 +0100

    first smart-brain-api commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const knex = require('knex');
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/',(req,res) => {
+    db.select('*').from('users')
+    .then(user =>  res.json(user))
+})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db,bcrypt,saltRounds)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+
+app.listen(3000, () => {
+    console.log('server is running')
+})
+
+/*
+
+/ --> res = this working
+/ signin --> POST = success/fail
+/register --> POST = user
+/profile/:uderId --> GET = user
+/image --> Put
+
+*/
